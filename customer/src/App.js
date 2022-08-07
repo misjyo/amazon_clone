4157bdd9a7022f63a70802a3ee8da524c3557e40
jyoti <mishrajyoti356@gmail.com>
2022-08-07 22:07:00 +0530

    firstcommit

---
+++ customer/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbaar from './components/header/Navbaar';
+import Maincomp from './components/home/Maincomp';
+import Newnav from './components/newnavbar/Newnav';
+
+function App() {
+  return (
+    <>
+<Navbaar/>
+<Newnav/>
+<Maincomp/>
+<Footer/>
+    </>
+  );
+}
+
+export default App;
